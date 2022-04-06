acd67f43bb41e0a84f1cce70795c58a715c0695a
ethanlyon <enl1@rice.edu>
2022-04-05 19:06:33 -0500

    Got basic multiplayer movement

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std;
-use std::ops;
-
-const R: usize = 4;
-const C: usize = 4;
+use rand::{thread_rng, Rng};
+const R: usize = 10;
+const C: usize = 10;
+const N_PLAYERS: usize = 7;
 
 fn print_grid(gr: &[[i32 ; C] ; R]){
     for i in 0..R {
@@ -20,34 +20,64 @@ fn search(i:usize, j:usize, gr: &mut [[i32 ; C] ; R]){
     if i < R - 1 {search(i + 1, j, gr);}
 }
 
-fn clamp<T>(value: &mut T, low: T, high: T) {
+fn clamp(value: &mut i32, low: i32, high: i32) {
     if *value < low {*value = low;}
     else if *value > high {*value = high;}
 }
+
 struct MainState{
-    p1: (i32, i32, i32, char),
-    p2: (i32, i32, i32, char),
+    players : [[i32; 3]; N_PLAYERS],
 }
 
 impl MainState {
-    pub fn new(s1: char, s2: char) -> Self {
+    pub fn new() -> Self {
         MainState 
         { 
-            p1: (0,0,0, s1), p2: ((R - 1) as i32, (C - 1) as i32, 0, s2) 
+            players: [[0; 3] ; N_PLAYERS],
         }
     }
 
-    pub fn random_walk() {
-        p1.1 += 
-        p2.2 +=
+    pub fn random_walk(&mut self){
+        let create_random_dir = |num| {let rng:i32 = thread_rng().gen_range(-1..2); num + rng};
+        for i in 0..N_PLAYERS{
+            let val = self.players[i];
+            let mut x1 = create_random_dir(val[0]);
+            let mut y1 = create_random_dir(val[1]);
+            clamp(&mut x1, 0, (R - 1) as i32);
+            clamp(&mut y1, 0, (C - 1) as i32);
+            //self.players[i] = [x1, y1, self.players[i][2]];
+
+            self.players[i][0] = x1;
+            self.players[i][1] = y1;
+            //self.players[i] = [x1, y1, self.players[i][2]];
+        }
+    }
+
+    pub fn update_grid(&self, grd: &mut [[i32 ; C] ; R]){
+        *grd = [[0i32 ; C] ; R];
+        for i in 0..N_PLAYERS {
+            let x = self.players[i][0] as usize;
+            let y = self.players[i][1] as usize;
+            grd[x][y] = (i+1) as i32;
+        }
     }
 }
 fn main() {
     let mut grid:[[i32 ; C] ; R] = [[0i32; C] ; R];
     print_grid(&grid);
         //print!("{}[2J", 27 as char);
-    print!("{}[2J", 27 as char);
-    search(0 ,0, &mut grid);
-    print_grid(&grid);
-    std::thread::sleep_ms(400);
+
+    
+
+    let mut rng = thread_rng();
+    let n: i32 = thread_rng().gen_range(-1..2);
+    let mut state = MainState::new();
+    loop{
+        print!("{}[2J", 27 as char);
+        print_grid(&grid);
+        state.random_walk();
+        state.update_grid(&mut grid);
+        println!("{:?}", state.players[1]);
+        std::thread::sleep_ms(500);
+    }
 }
